d12e949fb2cfdd29123fdd6f26d245ab18cc1f21
VIJAYRAJ RAHUL E B <vijayrajrahul515@gmail.com>
2025-11-17 15:15:47 +0530

    init

---
+++ application/vendor/nwidart/laravel-modules/src/Commands/stubs/nextloop-module-stubs/views/SCAFFOLDING/modals/show.blade.php
@@ -0,0 +1,6 @@
+<!--[todo] 
+    - **IMPORTANT** - Follow the instructions fully
+    - the current content in this file is example scaffolding (follow it as applicable) and complete the file
+    - remove this comment when done
+-->
+
